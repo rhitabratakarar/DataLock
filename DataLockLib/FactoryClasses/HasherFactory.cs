e7aedeedfc05326cac95e071402b4d1a27bbc49a
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-03 10:38:04 +0530

    added hasher factory

---
+++ DataLockLib/FactoryClasses/HasherFactory.cs
@@ -0,0 +1,13 @@
+﻿using DataLockLib.Classes;
+using DataLockLib.Interfaces;
+
+namespace DataLockLib.FactoryClasses
+{
+    public class HasherFactory
+    {
+        public IHasher GetHasher()
+        {
+            return new SHA256Hasher();
+        }
+    }
+}
