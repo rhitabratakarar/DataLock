4b92f1a2abad304877b2cc96b2a4e357d328b3dd
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-03 10:22:08 +0530

    added a credential Model to represent in database

---
+++ DataLockLib/Models/Credential.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLockLib.Models
+{
+    public class Credential
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        [Required]
+        public string Password { get; set; } = "";
+        public string? Notes { get; set; }
+        public string? URL { get; set; }
+        [Required]
+        public string Name { get; set; } = "";
+    }
+}
