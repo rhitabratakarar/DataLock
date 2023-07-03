4915dfcefde4ccf3ab55cf84e7d386aa0b1d6cd2
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-03 10:32:44 +0530

    added factory class of decrypter

---
+++ DataLockLib/FactoryClasses/DecrypterFactory.cs
@@ -0,0 +1,12 @@
+﻿using DataLockLib.Classes;
+using DataLockLib.Interfaces;
+namespace DataLockLib.FactoryClasses
+{
+    public class DecrypterFactory
+    {
+        public IDecrypter GetDecrypter()
+        {
+            return new AESDecrypter();
+        }
+    }
+}
