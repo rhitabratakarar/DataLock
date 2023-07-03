6630e9751e5f4397454cfb388d33e5b7d73f7611
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-03 10:36:33 +0530

    added encrypter factory

---
+++ DataLockLib/FactoryClasses/EncrypterFactory.cs
@@ -0,0 +1,13 @@
+﻿using DataLockLib.Interfaces;
+using DataLockLib.Classes;
+
+namespace DataLockLib.FactoryClasses
+{
+    public class EncrypterFactory
+    {
+        public IEncrypter GetEncrypter()
+        {
+            return new AESEncrypter();
+        }
+    }
+}
