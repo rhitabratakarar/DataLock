33b3ed5fd9415958b8903593a53ef80a1077405b
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-03 10:25:08 +0530

    renamed Decrypter -> AESDecrypter

--- DataLockLib/Classes/AESDecrypter.cs
+++ DataLockLib/Classes/AESDecrypter.cs
@@ -3,7 +3,7 @@ using DataLockLib.Interfaces;
 
 namespace DataLockLib.Classes
 {
-    public class Decrypter : IDecrypter
+    public class AESDecrypter : IDecrypter
     {
         public string GetDecryptedString(byte[] cipherText, byte[] Key, byte[] IV)
         {
@@ -38,7 +38,6 @@ namespace DataLockLib.Classes
         }
         public string GetDecryptedString(string stringCipherText, byte[] Key, byte[] IV)
         {
-            // converting string to bytes
             string[] hexArray = stringCipherText.Split('-');
             List<byte> byteList = new List<byte>();
             foreach (string hex in hexArray)
@@ -47,7 +46,6 @@ namespace DataLockLib.Classes
             }
             byte[] cipherText = byteList.ToArray();
 
-            // after conversion.
             if (cipherText == null || cipherText.Length <= 0)
                 throw new ArgumentNullException("cipherText");
             if (Key == null || Key.Length <= 0)
