53ec6190a52566f1623fe93d9eaa53e2d76a43f2
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-03 10:26:19 +0530

    Refactored references

--- DataLockTests/CryptoTest.cs
+++ DataLockTests/CryptoTest.cs
@@ -17,10 +17,10 @@ namespace DataLockTests
             byte[] hashedPass = hasher.GetHashedMessage(password, 32);
             byte[] hashedVect = hasher.GetHashedMessage(initVector, 16);
 
-            IEncrypter encrypter = new Encrypter();
+            IEncrypter encrypter = new AESEncrypter();
             string encryptedMessage = encrypter.GetEncryptedString(message, hashedPass, hashedVect);
 
-            IDecrypter decrypter = new Decrypter();
+            IDecrypter decrypter = new AESDecrypter();
             string decryptedMessage = decrypter.GetDecryptedString(encryptedMessage, hashedPass, hashedVect);
 
             Assert.True(message.Equals(decryptedMessage));
