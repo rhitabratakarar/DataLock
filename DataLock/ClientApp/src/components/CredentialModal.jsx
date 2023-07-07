f6565181e371c23ccacac0d9be2cadb2fbd1bf15
VikramAditya <deepkumarkhamaru@gmail.com>
2023-07-07 12:10:34 +0530

    credential functions footer wireframe

--- DataLock/ClientApp/src/components/CredentialModal.jsx
+++ DataLock/ClientApp/src/components/CredentialModal.jsx
@@ -3,13 +3,14 @@ import { ColorContext } from "../contexts";
 import InfoContainer from "./InfoContainer";
 import { AiOutlineLeft } from "react-icons/ai";
 import { GoPencil } from "react-icons/go";
+import { BiSearchAlt } from "react-icons/bi";
 
 export default function CredentialModal() {
   const colors = useContext(ColorContext);
   return (
     <InfoContainer className="py-3">
       <div
-        className="w-10/12 h-full flex flex-col items-center rounded-lg border-b-2 border-gray-600"
+        className="w-10/12 h-full flex flex-col items-center rounded-lg border-b-2 border-gray-600 pb-2"
         style={{ backgroundColor: colors.tertiary }}
       >
         <div className="flex flex-row items-center justify-between w-11/12 mb-3 mt-3 drop-shadow-lg">
@@ -53,7 +54,6 @@ export default function CredentialModal() {
             style={{ backgroundColor: colors.quaternary }}
             value="somerandomvalue"
           />
-
           <input
             type="url"
             placeholder="Url"
@@ -61,7 +61,6 @@ export default function CredentialModal() {
             disabled={true}
             style={{ backgroundColor: colors.quaternary }}
           />
-
           <textarea
             cols={20}
             rows={10}
@@ -70,7 +69,49 @@ export default function CredentialModal() {
             disabled={true}
             style={{ backgroundColor: colors.quaternary }}
           ></textarea>
-        </div>
+          
+          </div>
+          <div 
+            className="flex flex-row justify-center items-center w-11/12 h-[40px] rounded-md border-2 border-solid border-gray-800" 
+            // style={{ backgroundColor: colors.primary}}
+          >
+            <button
+          className="w-3/12 h-[36px] flex justify-center items-center mx-2"
+          style={{
+            borderRadius: "4px",
+            backgroundColor: colors.quaternary,
+          }}
+        >
+          <BiSearchAlt size={30}/>
+        </button>
+        <button
+          className="w-3/12 h-[36px] flex justify-center items-center mx-2"
+          style={{
+            borderRadius: "4px",
+            backgroundColor: colors.quaternary,
+          }}
+        >
+          <BiSearchAlt size={30}/>
+        </button>
+        <button
+          className="w-3/12 h-[36px] flex justify-center items-center mx-2"
+          style={{
+            borderRadius: "4px",
+            backgroundColor: colors.quaternary,
+          }}
+        >
+          <BiSearchAlt size={30}/>
+        </button>
+        <button
+          className="w-3/12 h-[36px] flex justify-center items-center mx-2"
+          style={{
+            borderRadius: "4px",
+            backgroundColor: colors.quaternary,
+          }}
+        >
+          <BiSearchAlt size={30}/>
+        </button>
+          </div>
       </div>
     </InfoContainer>
   );
