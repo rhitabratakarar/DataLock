a8d5d8fda4590087317a4ffe05988c1c96a9102d
VikramAditya <deepkumarkhamaru@gmail.com>
2023-07-21 16:50:30 +0530

    check kr to eta thik hbe kina

--- DataLock/ClientApp/src/colorPalletePicker.js
+++ DataLock/ClientApp/src/colorPalletePicker.js
@@ -1,5 +1,5 @@
 function getColorPalleteStringFromURL(url) {
-  let originalString = "6e85b7b2c8dfc4d7e0f8f9d7";
+  let originalString = "2c36393f4e4fa27b5cdcd7c9";
   
   if (url) {
     const stringList = url.split("/");
--- DataLock/ClientApp/src/components/CredentialSearch.jsx
+++ DataLock/ClientApp/src/components/CredentialSearch.jsx
@@ -6,14 +6,14 @@ export default function CredentialSearch() {
   const colors = useContext(ColorContext);
   useEffect(() => {
     const searchBox = document.getElementById("search");
-    searchBox.style.boxShadow = `0 30px 30px ${colors.primary}`;
+    searchBox.style.boxShadow = `0 100px 60px ${colors.primary}`;
     searchBox.style.backdropFilter = "blur(12px)";
     searchBox.style.background = `linear-gradient(180deg, ${colors.primary}00, ${colors.primary})`;
   });
   return (
     <div
       id="search"
-      className="w-[calc(100vw+100px)] min-w-[calc(100vw+100px)] h-[85px] min-h-[85px] items-center flex md:rounded-md md:mt-3 absolute -top-[40px] bg-transparent overflow-hidden"
+      className="w-[calc(100vw+100px)] min-w-[calc(100vw+100px)] h-[85px] min-h-[85px] items-center flex md:rounded-md md:mt-3 absolute -top-[120px] bg-transparent overflow-hidden"
       style={{ backgroundColor: colors.primary }}
     >
     </div>
