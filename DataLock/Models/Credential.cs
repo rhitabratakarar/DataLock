cff583c588bf6c9e8ee9c444c2e6797e52f505db
VikramAditya <deepkumarkhamaru@gmail.com>
2023-07-31 18:45:46 +0530

    Sample credential generator

---
+++ DataLock/Models/Credential.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLock.Models
+{
+    public class Credential
+    {
+        [Key]
+        public Guid CredentialId { get; } = Guid.NewGuid();
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        [Required]
+        public string Password { get; set; } = "";
+        public string? Notes { get; set; }
+        public string? URL { get; set; }
+        [Required]
+        public string Name { get; set; } = "";
+    }
+}
